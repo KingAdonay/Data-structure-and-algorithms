6a308b5ca3cefea28a38ae61f79dba74c94d62fe
Adonay Tezera <eshetuadonayad@gmail.com>
2025-02-09 20:55:50 +0000

    Baseball game problem solution (682)

---
+++ stack/BaseballGame.py
@@ -0,0 +1,31 @@
+from typing import List
+
+
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for val in operations:
+            if stack and val == 'C':
+                stack.pop()
+            elif stack and val == 'D':
+                d = stack[-1] * 2
+                stack.append(d)
+            elif len(stack) > 1 and val == '+':
+                stack.append(stack[-1] + stack[-2])
+            else:
+                stack.append(int(val))
+       
+        sum = 0
+        for i in range(len(stack)):
+            sum += stack[i]
+        
+        return sum
+
+# Tests
+sol = Solution()
+# Testcase 1: ["5","2","C","D","+"]
+sum = sol.calPoints(["5","2","C","D","+"])
+print(sum == 30)
+# Testcase 2: ["5","-2","4","C","D","9","+","+"]
+sum = sol.calPoints(["5","-2","4","C","D","9","+","+"])
+print(sum == 27)
