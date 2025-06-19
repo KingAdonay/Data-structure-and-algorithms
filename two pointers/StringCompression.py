bd68d00d8ebdfb6e690b418eba415dcea22b305f
Adonay Tezera <eshetuadonayad@gmail.com>
2025-06-20 00:09:21 +0100

    Clean up string compression problem solution

--- two pointers/StringCompression.py
+++ two pointers/StringCompression.py
@@ -5,33 +5,35 @@ class Solution:
     def compress(self, chars: List[str]) -> int:
         # ["a","a","b","b","c","c","c"]
         n = len(chars)
-        i = 0
-        curr_char = chars[0]
-        count = 0
-        for c in chars:
-            if curr_char == c:
+        write = 0
+        read = 0
+        while read < n:
+            curr_char = chars[read]
+            count = 0
+
+            while read < n and curr_char == chars[read]:
                 count += 1
-            else:
-                chars[i] = curr_char
-                i += 1
-                if count > 1:
-                    count_str = str(count)
-                    for s in count_str:
-                        chars[i] = s
-                        i+=1
-                curr_char = c
-                count = 1
-        if curr_char:
-            chars[i] = curr_char
-            i += 1
+                read += 1
+            
+            
+            chars[write] = curr_char
+            write += 1
             if count > 1:
                 count_str = str(count)
                 for s in count_str:
-                    chars[i] = s
-                    i+=1
-        return i
+                    chars[write] = s
+                    write += 1
+
+        return write
+
 
 # Tests
 sol = Solution()
 # Testcase 1: ["a","a","b","b","c","c","c"] => ["a","2","b","2","c","3"]
+chars = ["a","a","b","b","c","c","c"]
+count = sol.compress(chars)
+print(chars[:count] == ["a","2","b","2","c","3"])
 # Testcase 2: ["a","b","b","b","b","b","b","b","b","b","b","b","b"] => ["a","b","1","2"]
+chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+count = sol.compress(chars)
+print(chars[:count] == ["a","b","1","2"])
