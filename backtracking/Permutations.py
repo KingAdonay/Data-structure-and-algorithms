f9b2628425b40a4f80c4c3d1b9383d2a8b6fb4bd
Adonay Tezera <eshetuadonayad@gmail.com>
2026-02-02 23:28:30 +0000

    Permutation problem solution - backtracking (46)

--- backtracking/Permutations.py
+++ backtracking/Permutations.py
@@ -1,5 +1,14 @@
 from typing import List
 
+'''
+    46. Permutations
+    
+    Solution using Backtracking
+    
+    Time complexity: O(N * N!)
+    Space complexity: O(N!)
+'''
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         result = []
