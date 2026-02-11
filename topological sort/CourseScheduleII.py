7d556efc1987f2e26c8a4ff05106d74ce6fed4e5
Adonay Tezera <eshetuadonayad@gmail.com>
2026-02-11 21:35:23 +0000

    Add intution, approach and complexity details

--- topological sort/CourseScheduleII.py
+++ topological sort/CourseScheduleII.py
@@ -1,6 +1,25 @@
 from typing import List
 from collections import defaultdict, deque
 
+'''
+    210. Course Schedule II
+    Intuition:
+    We can use topological sorting to determine the order of courses to take based on their prerequisites.
+    If there is a cycle in the graph, it means that it's impossible to complete all courses
+    
+    Approach:
+    1. Create a graph representation of the courses and their prerequisites using an adjacency list.
+    2. Calculate the in-degree (number of prerequisites) for each course.
+    3. Use a queue to perform a breadth-first search (BFS) starting with courses that have an in-degree of 0 (no prerequisites).
+    4. For each course taken, reduce the in-degree of its neighboring courses (courses that depend on it) by 1.
+    5. If any neighboring course's in-degree becomes 0, add it to the queue.
+    6. Keep track of the order of courses taken. If the number of courses taken is equal to numCourses, return the order. Otherwise, return an empty list. 
+    
+    Time complexity: O(V + E) where V is the number of courses and E is the number of prerequisites
+    Space complexity: O(V + E) for the graph representation and in-degree array
+'''
+
+
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         order = []
