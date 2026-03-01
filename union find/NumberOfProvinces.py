e4cae08e4891478edc5b8fb5dab75789c3312b50
Adonay Tezera <eshetuadonayad@gmail.com>
2026-03-01 00:19:05 +0000

    Update number of provinces problem solution

--- union find/NumberOfProvinces.py
+++ union find/NumberOfProvinces.py
@@ -5,46 +5,65 @@ from typing import List
 
 Group the cities into provices based on the values in the isConnected matrix using union find by tracking parents,
 and count the number of parents with negative wights at the end. That indicates the head nodes (provinces).
+
+Time Complexity: O(n^2) due to the nested loop to iterate through the isConnected matrix.
+Space Complexity: O(n) for the union find data structure to track the provinces.
 '''
+
+class Helper:
+    def __init__(self, n: int):
+        self.n = n
+        self.provinces = [-1] * n
+    
+    def find(self, node: int) -> int:
+        parent = self.provinces[node]
+        if parent < 0:
+            return node, parent
+        
+        while self.provinces[parent] >= 0:
+            parent = self.provinces[parent]
+        
+        self.provinces[node] = parent # Path compression
+        
+        return parent, self.provinces[parent]
+    
+    def union(self, node1, node2):
+        parent1, weight1 = self.find(node1)
+        parent2, weight2 = self.find(node2)
+
+        if parent1 == parent2:
+            return
+
+        if weight1 <= weight2:
+            self.provinces[node2] = parent1
+            self.provinces[parent2] = parent1
+            self.provinces[parent1] += weight2
+        else:
+            self.provinces[node1] = parent2
+            self.provinces[parent1] = parent2
+            self.provinces[parent2] += weight1
+            
+    
+    def get_unique_provinces_count(self) -> int:
+        res = 0
+        for i in range(self.n):
+            if (self.provinces[i] < 0):
+                res += 1
+        return res
+
 
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         n = len(isConnected)
-        parents = [-1] * n
-
-        def find(idx: int) -> int:
-            if (parents[idx] < 0):
-                return idx, parents[idx]
-            return find(parents[idx])
-        
-        def union(city1: int, city2: int) -> None:
-            parent1, weight1 = find(city1)
-            parent2, weight2 = find(city2)
-
-            if parent1 == parent2:
-                return
-
-            if weight1 <= weight2:
-                parents[city2] = parent1
-                parents[parent1] += weight2
-                parents[parent2] = parent1
-            else:
-                parents[city1] = parent2
-                parents[parent2] += weight1
-                parents[parent1] = parent2
+        helper = Helper(n)
         
         for i in range(n):
             for j in range(n):
                 if isConnected[i][j] == 1:
-                    union(i, j)
-
-        res = 0
-        for i in range(n):
-            if (parents[i] < 0):
-                res += 1
+                    helper.union(i, j)
         
-        return res
-    
+        return helper.get_unique_provinces_count()
+
 # Tescases:
 s = Solution()
 print(s.findCircleNum([[1,1,0],[1,1,0],[0,0,1]]) == 2) # 2
